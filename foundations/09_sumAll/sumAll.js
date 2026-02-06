4980196b9de84e05783a9098eac23bf74a74994e
Valtteri Laakso <valtteri.laakso@tuni.fi>
2026-02-06 22:35:01 +0200

    Add exercise 6-11 solutions.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    if (start < 0 || end < 0) return "ERROR";
 
+    const smaller = Math.min(start, end);
+    const bigger = Math.max(start, end);
+
+    let sum = 0;
+    for (let i = smaller; i <= bigger; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
