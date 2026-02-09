880f22534c94cf146a9d5264b0171ccf34aa5df1
Valtteri Laakso <valtteri.laakso@tuni.fi>
2026-02-09 17:38:58 +0200

    Add exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(f, s) { /* f = first number, s = second number */
+  return f+s;
 };
 
-const subtract = function() {
-	
+const subtract = function(f,s) {
+	return f-s;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  if (arr.length === 0) { return 0;}
+  for(let i = 0; i < arr.length; i++){
+    sum = sum + arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let mult = 1;
+  for ( let i = 0; i < arr.length; i++) {
+    mult = mult * arr[i];
+  }
+  return mult;
 
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) return NaN;
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
